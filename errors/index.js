ae14cfc4eb042244506b404a056032fb4dbe1e19
Rengkat <successinmotion11@gmail.com>
2025-02-18 21:57:50 +0100

    index and not found error

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const BadRequestError = require("./bad-request");
+const CustomApiError = require("./custom-error");
+const NotFoundError = require("./not-found");
+
+module.exports = {
+  CustomApiError,
+  BadRequestError,
+  NotFoundError,
+};
