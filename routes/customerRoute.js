a3ded49057b5345723c0098919e0aba3d1d0e5fb
Rengkat <successinmotion11@gmail.com>
2025-02-18 22:32:33 +0100

    route add

---
+++ routes/customerRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {
+  addCustomer,
+  getAllCustomers,
+  updateCustomer,
+  deleteCustomer,
+} = require("../controller/customerController");
+const router = express.Router();
+router.route("/").post(addCustomer).get(getAllCustomers);
+router.route("/customerId").patch(updateCustomer).delete(deleteCustomer);
+module.exports = router;
